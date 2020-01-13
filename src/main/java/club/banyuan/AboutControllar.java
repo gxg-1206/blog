51f9176a18c671b61de870604a8f7c160d877dd4
edz <edz@gxgdeMacBook-Air.local>
2020-01-13 13:11:14 +0800

    1st controller

--- src/main/java/club/banyuan/AboutControllar.java
+++ src/main/java/club/banyuan/AboutControllar.java
@@ -1,10 +1,7 @@
 package club.banyuan;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class AboutApplication {
-    @GetMapping("/about")
     String
 }
--- src/main/java/club/banyuan/BlogApplication.java
+++ src/main/java/club/banyuan/BlogApplication.java
@@ -1,24 +1,14 @@
 package club.banyuan;
 
-import club.banyuan.bean.Blog;
-import club.banyuan.bean.User;
-import club.banyuan.dao.BlogDao;
-import club.banyuan.dao.CommentDao;
-import club.banyuan.dao.UserDao;
-import club.banyuan.service.BlogService;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
-import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@MapperScan("club.banyuan.dao")
+//@MapperScan("club.banyuan.dao")
+@Controller
 public class BlogApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(BlogApplication.class, args);
